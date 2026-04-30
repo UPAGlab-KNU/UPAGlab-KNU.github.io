5581c6d0273bf6fd9388eeba4c709ac799d41a98
UPAGlab-KNU <UPAGlab-KNU@users.noreply.github.com>
2026-04-30 10:30:33 +0000

    Deploying to gh-pages from @ UPAGlab-KNU/UPAGlab-KNU.github.io@67dd1114934e9acd862967bdb124e4995c1951b0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "UPAGlab-KNU/UPAGlab-KNU.github.io-",
+    "data-repo": "UPAGlab-KNU/UPAGlab-KNU.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
