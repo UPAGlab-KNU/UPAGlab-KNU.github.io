dcf403f3c27e8b69bf4be0a334ae83e6a35ba4c3
UPAGlab-KNU <hanlp2082@gmail.com>
2026-05-02 06:28:15 +0000

    Deploying to gh-pages from @ UPAGlab-KNU/UPAGlab-KNU.github.io@fe73f3992aa9cc810a9100fd7ab685399340e327 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-people",
           title: "people",
           description: "Members of UPAG Lab",
